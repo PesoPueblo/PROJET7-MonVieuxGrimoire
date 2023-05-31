e7a94aaed74d73ad9d8c9768ff7e1de98d280ed2
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-05-31 18:29:13 +0200

    suppression modele ratings

--- models/ratings.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose');
-//création du modèle de note
-const ratings = mongoose.Schema({
-    userId : {type: String, required: true},
-    grade: {type: Number, required: true},
-});
-
-//exportation du modèle
-module.exports=mongoose.model('ratings', ratings)
