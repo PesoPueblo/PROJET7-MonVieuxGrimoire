99376440cf1f471b9bc789a3411eeed526877d11
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-06-06 09:48:19 +0200

    ajout rezise image avec sharp

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,11 +1,14 @@
 const multer = require ('multer');
+const sharp= require ('sharp');
 
+//on définis les mimetypes acccepté
 const  MIME_TYPES = {
     'image/jpg' : 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 
 }
+//configuration de multer
 const storage = multer.diskStorage({
     destination: (req,file,callback) => {
         callback(null, 'image')
@@ -16,4 +19,21 @@ const storage = multer.diskStorage({
         callback(null, name + Date.now()+ '.'+ extension);
     }
 });
-module.exports= multer({storage}).single('image');
+const upload = multer({storage}).single('image');
+
+//redimensionner les images
+const reziseImage = (req,res,next)=>{
+    if(!req.file) {
+        return next();
+    } else {
+        sharp(req.file.path)
+            .resize(400,500)
+            .toFile(req.file.path, (error, info) => {
+                if (error){
+                    res.status(500).json({error})
+                }
+            })
+    }
+};
+
+module.exports={upload, reziseImage};
--- routes/books.js
+++ routes/books.js
@@ -4,13 +4,13 @@ const auth = require('../middleware/auth')
 //importation des controlleurs 
 const BookCtrl = require('../controllers/book');
 //importation du  middleware multer
-const multer= require ('../middleware/multer-config')
+const {upload,reziseImage}= require ('../middleware/multer-config')
 
 const router = express.Router();
 
 //création des différentes routes de l'API
-router.post('/', auth, multer, BookCtrl.createNewBook);
-router.put('/:id', auth, multer, BookCtrl.modifyOneBook);
+router.post('/', auth, upload,reziseImage, BookCtrl.createNewBook);
+router.put('/:id', auth, upload,reziseImage, BookCtrl.modifyOneBook);
 router.delete('/:id',auth,  BookCtrl.deleteOneBook);
 router.get('/bestrating', BookCtrl.findBestRating);
 router.get('/:id', BookCtrl.getOneBook);
