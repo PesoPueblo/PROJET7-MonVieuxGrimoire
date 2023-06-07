568ea5a0b74983aa5415e659067628770728b820
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-06-07 11:26:28 +0200

    changement multer rezising si modification d'un livre sans changer les images

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -25,19 +25,24 @@ const upload = multer({storage}).single('image');
 //redimensionner les images
 const reziseImage = (req,res,next)=>{
     if(!req.file) {
-        error=>{res.status(500).json({error:'Aucune image trouvé'})};
+        next();
     } else {
+        //création d'un nouveau nom 
         const outputPath = req.file.path.replace(/\.[^/.]+$/, "") + "-resized.jpg";
         sharp(req.file.path)
+        //resize en taille voulu
             .resize(400,500)
+            //enregistrement sur le nouveau chemin 
             .toFile(outputPath, (error, info) => {
                 if (error){
                     res.status(500).json({info})
                 }else {
+                    //suppression du premier fichier créé par multer 
                     const odlPath= outputPath.split('-resized').join("");
-                    fs.unlink(odlPath,(err) => {   if (err) {res.status(501).json({err})};   console.log('Fichier supprimé !');});
+                    fs.unlink(odlPath,(err) => {   if (err) {res.status(501).json({err})}});
                 }
             });
+            //modification de la requête pour l'envoie
             req.file.filename= req.file.filename.replace(/\.[^/.]+$/, "") + "-resized.jpg";
             req.file.path=outputPath;
             next();
